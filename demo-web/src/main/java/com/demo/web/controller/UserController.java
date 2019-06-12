8ae7f1709a06c53ecb38f1aec0ab57efdde26fa4
100ME\wude <wude@100.me>
2019-06-12 13:20:48 +0800

    mybatis-code修改

--- demo-web/src/main/java/com/demo/web/controller/UserController.java
+++ demo-web/src/main/java/com/demo/web/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.demo.web.controller;
 
+import com.demo.dao.UserDao;
+import com.demo.domain.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * User控制器
@@ -12,9 +18,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = "user")
 public class UserController {
+    @Autowired
+    private UserDao userDao;
 
     @RequestMapping(value = "list")
     public String list() {
         return "user/users";
+    }
+
+    @GetMapping(value = "get/{id}")
+    @ResponseBody
+    public User list(@PathVariable int id) {
+        return userDao.selectById(id);
     }
 }
